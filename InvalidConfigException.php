4c9146497fff578d34d6d1a3b90adaba6d434f06
huangshunzhao <huangshunzhao@wanda.cn>
2017-01-12 16:53:02 +0800

    优化，增加参数

--- InvalidConfigException.php
+++ InvalidConfigException.php
@@ -7,15 +7,22 @@ namespace ffan\php\utils;
  */
 class InvalidConfigException extends \Exception implements Exception
 {
+    /**
+     * 配置出错的错误ID
+     */
     const ERROR_CODE = 2;
 
     /**
      * InvalidConfigException constructor.
-     * @param string $config_name
+     * @param string $config_name 配置名
+     * @param string $reason 原因
      */
-    public function __construct($config_name)
+    public function __construct($config_name, $reason = '')
     {
         $message = 'Invalid config:' . $config_name;
+        if (!empty($reason)) {
+            $message .= ' Reason:' . $reason;
+        }
         parent::__construct($message, self::ERROR_CODE);
     }
 }
